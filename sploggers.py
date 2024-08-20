b0f780c90d882efe8503d23378f6ea32463208e1
lorenzohz <zlorenzoh@gmail.com>
2024-08-19 22:44:22 -0300

    sploggers

---
+++ sploggers.py
@@ -0,0 +1,123 @@
+# Simulador de Arquitetura de Computadores
+
+""" 
+A arquitetura deve ter 32 registradores de uso geral de 64 bits cada (8 bytes);
+◦ Os registradores devem ser nomeados de r0 até r31;
+    ▪ Inicialmente todos eles devem conter o valor 0;
+◦ É obrigatório o uso dos registradores de estado/uso específico;
+    ▪ PC - Contador de programa;
+    ▪ RSP - Ponteiro de pilha;
+    ▪ RA - Endereço de retorno;
+    ▪ OF - Registrador de 1 bit para indicar se houve overflow na última operação; 
+
+Conjunto de instruções:
+• As instruções aritméticas e de desvio só podem ter seus operandos endereçados de duas maneiras:
+    ◦ Endereçamento direto por registrador;
+    ◦ Endereçamento por imediato;
+• As instruções de acesso à memória deverão ter um operando com endereçamento direto por registrador e um operando com endereçamento por deslocamento;
+• O formato e significado das instruções que deverão ser implementadas são exibidos na Tabela 1;
+• Todas as instruções deverão operar somente sobre os registradores de uso geral (r0, r1, ..., r31) e o ponteiro de pilha (RSP);
+◦ Ou seja, os registradores de Overflow (OF), contador de programa PC, e endereço de retorno (RA) não devem ser manipulados diretamente pelo
+programador assembly (algumas instruções irão realizar a manipulação de tais registradores);
+• As operações devem ser realizadas somente com valores inteiros;
+• As instruções deverão ser de tamanho fixo e deverão ter 64 bits cada (ou seja, 8 bytes);
+
+Memória:
+• A arquitetura deve ter memória estruturada em dois níveis, uma memória cache, separada em dados e instruções, e uma memória principal;
+• Cada endereço de memória (tanto da principal, quanto da cache de dados ou da cache de instruções) deverá armazenar 8 bytes (ou seja, uma instrução ou um inteiro).
+Obs: A limitação de 8 bytes não impede que as estruturas usadas para representar cada célula de memória possua mais informações
+• A cache deve ser associativa por conjunto:
+    ◦ O número de bytes por linha, linhas por conjunto e número total de conjuntos deve ser configurável;
+▪ O tamanho total da cache é dado pelo produto desses 3 elementos;
+◦ Use os mesmos valores para as caches de dados e instruções;
+◦ Para facilidade com a manipulação dos dados da memória, o número de bytes por linha deve ser múltiplo de 8;
+    ▪ Caso a dupla prefira, pode utilizar o número de palavras por linha.
+    ▪ O valor máximo para bytes por linha deve ser de 1024 (no caso de palavras por linha, o valor máximo de palavras por linha deve ser 128);
+Obs: A limitação de 1024 bytes por linha é referente aos dados armazenados na cache. Na implementação do simulador, estruturas auxiliares podem ser modeladas para auxiliar o controle da cache;
+◦ A memória principal deverá ter seu tamanho configurável em número de bytes (também deve ser múltiplo de 8) e deverá ser maior que a memória cache;
+    ▪ Caso a dupla prefira, a memória principal também pode ser configurada em número de palavras da memória.
+    ▪ A medida adotada deve ser a mesma para cache e memória principal
+    • Se a cache é configurada por palavras, a memória principal também deve ser medida em palavras;
+    • Se a cache é configurada por bytes, a memória principal deverá ser medida em bytes
+    • Caso a dupla deseje, esta opção pode ser um argumento do programa, porém, isso não é obrigatório;
+
+Entradas:
+• A entrada principal do programa deverá ser um arquivo de texto contendo um conjunto de instruções a serem executadas, onde cada instrução ocupa uma linha do arquivo.
+• Alguns exemplos estão disponíveis no mesmo tópico desta especificação.
+• As configurações tais como memória cache e memória principal devem ser feitas preferencialmente por argumentos passados na linha de comando, ou então um arquivo de configuração que informa esses valores
+
+Saídas:
+• A cada ciclo de instrução deverão ser exibidos na tela:
+◦ Os valores armazenados na memória cache;
+    ▪ Não é necessário mostrar endereços que não estejam ocupados com dados que façam parte do programa;
+    ▪ Os valores exibidos devem ser separados por dados e instruções;
+    ▪ As instruções podem ser mostradas na sua forma textual;
+◦ Os valores armazenados em cada endereço da memória;
+    ▪ As instruções podem ser mostradas na sua forma textual;
+◦ Os valores armazenados em cada um dos registradores de uso geral;
+◦ Os valores armazenados em cada um dos registradores de controle de
+estado;
+"""
+
+# Configurações da memória cache
+N_BYTES_POR_LINHA = 8 # Deve ser múltiplo de 8 e menor que 1024 (cada instrução ou inteiro ocupa no máximo 8 bytes)
+N_LINHAS_POR_CONJUNTO = 4
+N_CONJUNTOS = 4
+
+# Configurações da memória principal
+N_BYTES_MEMORIA_PRINCIPAL = 128 # Deve ser múltiplo de 8 e maior que a memória cache
+
+
+def inicializa_memoria_cache() -> dict:
+    conjuntos: dict = {}
+    for i in range(N_CONJUNTOS):
+        conjuntos[i] = []
+        for j in range(N_LINHAS_POR_CONJUNTO):
+            conjuntos[i].append([0] * N_BYTES_POR_LINHA)
+    return conjuntos
+
+def inicializa_memoria_principal() -> list:
+    return [0] * N_BYTES_MEMORIA_PRINCIPAL
+
+def main():
+
+    # Variáveis globais
+    # Registradores de uso geral
+    r0 = 0
+    r1 = 0
+    r2 = 0
+    r3 = 0
+    r4 = 0
+    r5 = 0
+    r6 = 0
+    r7 = 0
+    r8 = 0
+    r9 = 0
+    r10 = 0
+    r11 = 0
+    r12 = 0
+    r13 = 0
+    r14 = 0
+    r15 = 0
+    r16 = 0
+    r17 = 0
+    r18 = 0
+    r19 = 0
+    r20 = 0
+    r21 = 0
+    r22 = 0
+    r23 = 0
+    r24 = 0
+    r25 = 0
+    r26 = 0
+    r27 = 0
+    r28 = 0
+    r29 = 0
+    r30 = 0
+    r31 = 0
+
+    # Registradores de estado/uso específico
+    PC = 0
+    RSP = 0
+    RA = 0
+    OF = 0
